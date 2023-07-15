20629084e1878160dfd061f7805c726e0369ffe0
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-07-15 10:14:59 +0530

    Updated the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,34 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()]
+  // base: '.', // Set a path prefix.
+  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
+  trailingSlash: 'always', // Use to always append '/' at end of url
+  // Important!
+  // Only official '@astrojs/*' integrations are currently supported by Astro.
+  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
+  // with 'astro build' command.
+  experimental: {
+    integrations: true,
+  },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'monokai',
+    },
+  },
+  integrations: [
+    react(),
+    tailwind({}),
+    sitemap(),
+    robotsTxt(),
+    astroImageTools,
+  ],
 });
