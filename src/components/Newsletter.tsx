f04a37411709b948e9850f2be7989594bd2ba0c3
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-07-18 22:31:07 +0530

    Netlify form submission added for the newsletter

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -14,8 +14,17 @@ const Newsletter = (props: INewsletterProps) => (
     </div>
 
     <div className="w-full sm:w-5/12">
-      <form className="flex rounded-full bg-slate-800 px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-600 hover:ring-2 hover:ring-cyan-600">
-        <input className="w-full appearance-none bg-slate-800 focus:outline-none" />
+      <form
+        name="contact"
+        method="POST"
+        className="flex rounded-full bg-slate-800 px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-600 hover:ring-2 hover:ring-cyan-600"
+        data-netlify="true"
+      >
+        <input
+          type="email"
+          name="email"
+          className="w-full appearance-none bg-slate-800 focus:outline-none"
+        />
 
         <button
           className="ml-2 shrink-0 rounded-full bg-gradient-to-br from-sky-500 to-cyan-400 px-3 py-1 text-sm font-medium hover:from-sky-700 hover:to-cyan-600 focus:outline-none focus:ring-2 focus:ring-cyan-600/50"
