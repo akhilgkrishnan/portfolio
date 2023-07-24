ffd5c480f04ba1628213041935a3a54da7121495
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-07-24 12:17:02 +0530

    Footer updated

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -5,7 +5,12 @@ const Footer = () => (
   <Section>
     <div className="border-t border-gray-600 pt-5">
       <div className="text-sm text-gray-800 dark:text-gray-200">
-        © Copyright {new Date().getFullYear()} by {AppConfig.site_name}.
+        © Copyright {new Date().getFullYear()} by {AppConfig.site_name}. Made
+        with 🧡 and{' '}
+        <a href="https://astro.build" className="text-blue-400" target="_blank">
+          AstroJS
+        </a>
+        .
       </div>
     </div>
   </Section>
