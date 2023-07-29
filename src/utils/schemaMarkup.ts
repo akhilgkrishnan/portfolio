24c9f2bb988e9309a94d344b4555314204db4408
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-07-29 08:52:48 +0530

    Structure schema updated

---
+++ src/utils/schemaMarkup.ts
@@ -0,0 +1,29 @@
+export const schemaMarkup = {
+  '@context': 'https://schema.org/',
+  '@type': 'Person',
+  name: 'Akhil G Krishnan',
+  alternateName: 'Akhil Krishnan',
+  url: 'https://akhilgkrishnan.me',
+  image: 'https://avatars.githubusercontent.com/u/22231095?v=4',
+  sameAs: [
+    'https://facebook.com/akhilgkrishnan9800',
+    'https://twitter.com/mrakhilg',
+    'https://instagram.com/akhilgkrishnan',
+    'https://github.com/akhilgkrishnan',
+    'https://stackoverflow.com/users/10321356/akhil-g-krishnan',
+    'https://linkedin.com/in/akhilgkrishnan',
+    'https://akhilgkrishnan.github.io',
+    'https://scholar.google.com/citations?user=RHT03hwAAAAJ&hl=en',
+    'https://in.pinterest.com/akhilgkrishnan/',
+    'https://www.threads.net/@akhilgkrishnan',
+  ],
+  jobTitle: 'Senior Software Developer',
+  worksFor: {
+    '@type': 'Organization',
+    name: 'Saeloun Technologies Pvt. Ltd.',
+  },
+  alumniOf: {
+    '@type': 'EducationalOrganization',
+    name: 'Providence College of Engineering',
+  },
+};
