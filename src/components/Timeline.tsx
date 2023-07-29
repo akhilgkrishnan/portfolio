aa38a3c021f8050b21376c5363350b75fdabc36b
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-07-29 15:58:31 +0530

    padding fix in cv

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -10,7 +10,7 @@ const TimeLine = (props: ITimeLineProps) => (
       <span className="mt-1 block h-4 w-4 rounded-full bg-primary"></span>
       <span className="education__line block h-full w-[2px] translate-x-[7px] bg-primary"></span>
     </div>
-    <div className="experience__data bd-grid px-5">
+    <div className="experience__data bd-grid px-5 pb-5">
       <h3 className="mb-1 font-semibold">{props.title}</h3>
       <span className="text-sm font-light">{props.subtitle}</span>
       <p className="my-2 text-justify">{props.children}</p>
