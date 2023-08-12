5cb13f6aab16076b19c661846079b38133593f81
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-12 11:52:16 +0530

    Active nabar status added

--- src/components/NavMenuItem.tsx
+++ src/components/NavMenuItem.tsx
@@ -1,12 +1,25 @@
+import { NavbarContext } from '@partials/Navbar';
+import { useContext } from 'react';
+
 type INavMenuItemProps = {
   href: string;
   children: string;
 };
 
-const NavMenuItem = (props: INavMenuItemProps) => (
-  <li className="hover:text-gray-500 dark:hover:text-white">
-    <a href={props.href}>{props.children}</a>
-  </li>
-);
+const NavMenuItem = (props: INavMenuItemProps) => {
+  const path = useContext(NavbarContext);
+  const active = path === props.href;
+
+  return (
+    <a
+      href={props.href}
+      className={`inline-flex ${
+        active ? 'border-indigo-500' : 'border-transparent'
+      } items-center border-b-2  px-1 pt-1 font-medium text-gray-500 hover:border-gray-500 hover:text-gray-700 dark:text-gray-200`}
+    >
+      {props.children}
+    </a>
+  );
+};
 
 export default NavMenuItem;
--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -2,31 +2,40 @@ import DarkModeToggleButton from '@components/DarkModeToggleButton';
 import Logo from '@components/Logo';
 import NavMenuItem from '@components/NavMenuItem';
 import { Section } from 'astro-boilerplate-components';
+import { createContext } from 'react';
 
-const Navbar = () => (
-  <Section>
-    <div className="navbar flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between">
-      <div className="flex-1">
-        <a href="/" className="flex-none">
-          <Logo />
-        </a>
+export const NavbarContext = createContext('/');
+
+type NavbarProps = {
+  path: string;
+};
+
+const Navbar = (props: NavbarProps) => (
+  <NavbarContext.Provider value={props.path}>
+    <Section>
+      <div className="navbar flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between">
+        <div className="flex">
+          <a href="/" className="flex-none">
+            <Logo />
+          </a>
+        </div>
+        <ul className="flex items-center gap-x-3 font-medium text-gray-800 dark:text-gray-200">
+          <NavMenuItem href="/posts/">Blogs</NavMenuItem>
+          <NavMenuItem href="/projects/">Projects</NavMenuItem>
+          <NavMenuItem href="https://github.com/akhilgkrishnan">
+            GitHub
+          </NavMenuItem>
+          <NavMenuItem href="/cv/">Resume</NavMenuItem>
+          <NavMenuItem href="mailto:akhilgkrishnan4us@gmail.com">
+            Contact
+          </NavMenuItem>
+          <li>
+            <DarkModeToggleButton />
+          </li>
+        </ul>
       </div>
-      <ul className="flex items-center gap-x-3 font-medium text-gray-800 dark:text-gray-200">
-        <NavMenuItem href="/posts/">Blogs</NavMenuItem>
-        <NavMenuItem href="/projects/">Projects</NavMenuItem>
-        <NavMenuItem href="https://github.com/akhilgkrishnan">
-          GitHub
-        </NavMenuItem>
-        <NavMenuItem href="/cv/">Resume</NavMenuItem>
-        <NavMenuItem href="mailto:akhilgkrishnan4us@gmail.com">
-          Contact
-        </NavMenuItem>
-        <li>
-          <DarkModeToggleButton />
-        </li>
-      </ul>
-    </div>
-  </Section>
+    </Section>
+  </NavbarContext.Provider>
 );
 
 export { Navbar };
