1b7ce9873c2a1fb1edcfbbc6898254f03447602c
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-12 14:06:12 +0530

    Sticky navbar added

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -1,7 +1,6 @@
 import DarkModeToggleButton from '@components/DarkModeToggleButton';
 import Logo from '@components/Logo';
 import NavMenuItem from '@components/NavMenuItem';
-import { Section } from 'astro-boilerplate-components';
 import { createContext } from 'react';
 
 export const NavbarContext = createContext('/');
@@ -12,29 +11,27 @@ type NavbarProps = {
 
 const Navbar = (props: NavbarProps) => (
   <NavbarContext.Provider value={props.path}>
-    <Section>
-      <div className="navbar flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between">
-        <div className="flex">
-          <a href="/" className="flex-none">
-            <Logo />
-          </a>
-        </div>
-        <ul className="flex items-center gap-x-3 font-medium text-gray-800 dark:text-gray-200">
-          <NavMenuItem href="/posts/">Blogs</NavMenuItem>
-          <NavMenuItem href="/projects/">Projects</NavMenuItem>
-          <NavMenuItem href="https://github.com/akhilgkrishnan">
-            GitHub
-          </NavMenuItem>
-          <NavMenuItem href="/cv/">Resume</NavMenuItem>
-          <NavMenuItem href="mailto:akhilgkrishnan4us@gmail.com">
-            Contact
-          </NavMenuItem>
-          <li>
-            <DarkModeToggleButton />
-          </li>
-        </ul>
+    <nav className="navbar sticky top-0 z-10 mx-auto flex max-w-screen-lg flex-col gap-y-3 bg-slate-200 px-3 py-6 shadow  dark:bg-slate-800 sm:flex-row sm:items-center sm:justify-between">
+      <div className="flex">
+        <a href="/" className="flex-none">
+          <Logo />
+        </a>
       </div>
-    </Section>
+      <ul className="flex items-center gap-x-3 font-medium text-gray-800 dark:text-gray-200">
+        <NavMenuItem href="/posts/">Blogs</NavMenuItem>
+        <NavMenuItem href="/projects/">Projects</NavMenuItem>
+        <NavMenuItem href="https://github.com/akhilgkrishnan">
+          GitHub
+        </NavMenuItem>
+        <NavMenuItem href="/cv/">Resume</NavMenuItem>
+        <NavMenuItem href="mailto:akhilgkrishnan4us@gmail.com">
+          Contact
+        </NavMenuItem>
+        <li>
+          <DarkModeToggleButton />
+        </li>
+      </ul>
+    </nav>
   </NavbarContext.Provider>
 );
 
