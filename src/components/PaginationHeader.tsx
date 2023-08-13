7ba6c8869c05755df5f5bc65c037cddd4dc40101
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-13 10:19:32 +0530

    Light theme text issue fix

---
+++ src/components/PaginationHeader.tsx
@@ -0,0 +1,16 @@
+type IPaginationHeaderProps = {
+  title: string;
+  description: string;
+};
+
+const PaginationHeader = (props: IPaginationHeaderProps) => (
+  <div className="text-center">
+    <h1 className="text-3xl font-bold">{props.title}</h1>
+
+    <div className="mt-3 text-gray-700 dark:text-gray-200">
+      {props.description}
+    </div>
+  </div>
+);
+
+export { PaginationHeader };
