4f62668a4359c52ced33018ab3fb4975b769b195
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-14 22:07:13 +0530

    RSS fix

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,7 @@
-import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { AppConfig } from '@utils/AppConfig';
 
-export asyncconst get = async () =>
+export const get = async () =>
   rss({
     // `<title>` field in output xml
     title: AppConfig.title,
@@ -13,9 +13,7 @@ export asyncconst get = async () =>
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./posts/*.md')
-    ),
+    items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
