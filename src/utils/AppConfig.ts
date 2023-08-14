345adbebf166cbb50d50ea89324baab2fdc9b86b
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-14 12:33:22 +0530

    Title shortened

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,7 @@
 export const AppConfig = {
   site_url: 'https://akhilgkrishnan.me',
   site_name: 'Akhil G Krishnan',
-  title:
-    'Akhil G Krishnan | Software Developer | Open Source Contributor | CyberSecurity Researcher',
+  title: 'Akhil G Krishnan | Software Developer | Open Source Contributor',
   description:
     "Akhil G Krishnan's personal website. He is an Indian Software Developer, Open Source Contributor, AI/ML Enthusiast",
   keywords:
