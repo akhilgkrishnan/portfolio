aaf68230ff2a5b1bbc89bbfdc46e0fa7622154d0
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-10-12 14:47:20 +0530

    Website schema markup added

--- src/utils/schemaMarkup.ts
+++ src/utils/schemaMarkup.ts
@@ -1,6 +1,6 @@
 import { AppConfig } from './AppConfig';
 
-export const schemaMarkup = {
+export const personSchemaMarkup = {
   '@context': 'https://schema.org/',
   '@type': 'Person',
   name: AppConfig.site_name,
@@ -28,4 +28,27 @@ export const schemaMarkup = {
     '@type': 'EducationalOrganization',
     name: 'Providence College of Engineering',
   },
+};
+
+export const websiteSchemaMarkup = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  description:
+    'Software professional with over 4 years of experience in the IT industry.',
+  headline: 'Akhil G Krishnan',
+  image: `${AppConfig.site_url}/profile-pic.png`,
+  name: 'Akhil G Krishnan',
+  sameAs: [
+    'https://facebook.com/akhilgkrishnan9800',
+    'https://twitter.com/mrakhilg',
+    'https://instagram.com/akhilgkrishnan',
+    'https://github.com/akhilgkrishnan',
+    'https://stackoverflow.com/users/10321356/akhil-g-krishnan',
+    'https://linkedin.com/in/akhilgkrishnan',
+    'https://akhilgkrishnan.github.io',
+    'https://scholar.google.com/citations?user=RHT03hwAAAAJ&hl=en',
+    'https://in.pinterest.com/akhilgkrishnan/',
+    'https://www.threads.net/@akhilgkrishnan',
+  ],
+  url: 'https://akhilgkrishnan.me/',
 };
