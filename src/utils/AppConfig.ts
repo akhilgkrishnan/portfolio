29ce1a94ca0cad23f674a0def019a1dc566df29e
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-11-25 11:07:13 +0530

    Fixed parsing error

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -5,7 +5,7 @@ export const AppConfig = {
   description:
     "Explore Akhil G Krishnan's 4 years of full-stack development expertise, open source contributions, and passion for Ruby on Rails and React.",
   keywords:
-    'akhil, akhil g, akhil krishnan, akhil g krishnan, portfolio, sealoun, activatecare, , india, web developer, ethical hacker, cyber security researcher, rails developer, react developers, javascript', 'akhil g krishnan official website',
+    'akhil, akhil g, akhil krishnan, akhil g krishnan, portfolio, sealoun, activatecare, india, web developer, ethical hacker, cyber security researcher, rails developer, react developers, javascript, akhil g krishnan official website',
   author: 'Akhil G Krishnan',
   locale_region: 'en-us',
   locale: 'en',
