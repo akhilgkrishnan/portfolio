9653310a25c622856e060112b25c02b479555cf1
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2026-03-09 11:32:12 +0530

    rss fix

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,28 +1,21 @@
 import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
 import { AppConfig } from '@utils/AppConfig';
 import { getCollection } from 'astro:content';
 
-export const get = async () => {
+export async function GET(context: APIContext) {
   const posts = await getCollection('post');
 
   return rss({
-    // `<title>` field in output xml
     title: AppConfig.title,
-    // `<description>` field in output xml
     description: AppConfig.description,
-    // base URL for RSS <item> links
-    // SITE will use "site" from your project's astro.config.
-    site: import.meta.env.SITE,
-    // list of `<item>`s in output xml
-    // simple example: generate items for every md file in /src/pages
-    // see "Generating items" section for required frontmatter and advanced use cases
+    site: context.site ?? AppConfig.site_url,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blogs/${post.slug}/`,
     })),
-    // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
-};
+}
